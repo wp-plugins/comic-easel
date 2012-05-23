fbe5d55a013c26d370b3b289f75bb0c1d5488087
Frumph <Frumph@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-23 07:39:06 +0000

    Archive CSS changes
    git-svn-id: https://plugins.svn.wordpress.org/comic-easel/trunk@547860 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -93,7 +93,7 @@ function ceo_archive_list_single($chapter = 0, $order = 'ASC', $thumbnail = 0) {
 	}
 	foreach ($qposts as $qpost) {
 		$archive_count++;
-		$output .= '<li class="comic-list comic-list-'.$archive_count.'"><span class="comic-archive-date">'.get_the_time('M j, Y', $qpost->ID).'</span><span class="comic-archive-title"><a href="'.get_permalink($qpost->ID).'" rel="bookmark" title="'.__('Permanent Link:','comiceasel').' '.$qpost->post_title.'">'.$qpost->post_title.'</a></span></li>';
+		$output .= '<li class="comic-list comic-list-'.$archive_count.'"><span class="comic-archive-date">'.get_the_time('M d, Y', $qpost->ID).'</span><span class="comic-archive-title"><a href="'.get_permalink($qpost->ID).'" rel="bookmark" title="'.__('Permanent Link:','comiceasel').' '.$qpost->post_title.'">'.$qpost->post_title.'</a></span></li>';
 	}
 	$output .= '</ul>';
 	$output .= '<div style="clear:both;"></div></div>';
@@ -125,9 +125,11 @@ function ceo_archive_list_all($order = 'ASC', $thumbnail = 0) {
 				$output .= '<div class="comic-archive-thumbnail">'.get_the_post_thumbnail($qposts[0]->ID, 'thumbnail').'</div>';
 			}
 			$output .= '<div class="comic-archive-list-wrap">';
+			$css_alt = false;
 			foreach ($qposts as $qpost) {
 				$archive_count++;
-				$output .= '<li class="comic-list comic-list-'.$archive_count.'"><span class="comic-archive-date">'.get_the_time('M j, Y', $qpost->ID).'</span><span class="comic-archive-title"><a href="'.get_permalink($qpost->ID).'" rel="bookmark" title="'.__('Permanent Link:','comiceasel').' '.$qpost->post_title.'">'.$qpost->post_title.'</a></span></li>';
+				if ($css_alt) { $alternate = ' comic-list-alt'; $css_alt = false; } else { $alternate = ''; $css_alt=true; }
+				$output .= '<div class="comic-list comic-list-'.$archive_count.$alternate.'"><span class="comic-archive-date">'.get_the_time('M j, Y', $qpost->ID).'</span><span class="comic-archive-title"><a href="'.get_permalink($qpost->ID).'" rel="bookmark" title="'.__('Permanent Link:','comiceasel').' '.$qpost->post_title.'">'.$qpost->post_title.'</a></span></div>';
 			}
 			$output .= '</div>';
 			$output .= '<div style="clear:both;"></div></div>';
